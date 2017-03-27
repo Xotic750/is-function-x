1c589e19c3f2ed6914e9d82f250e4461c451cd41
Graham Fairweather <graham.fairweather@ispy.se>
2017-03-27 13:36:13 +0200

    Remove yarn and jscs, cleanup

--- index.js
+++ index.js
@@ -22,41 +22,16 @@
  * alt="npm version" height="18">
  * </a>
  *
- * isFunction module. Determine whether a given value is a function object.
+ * Determine whether a given value is a function object.
  *
- * <h2>ECMAScript compatibility shims for legacy JavaScript engines</h2>
- * `es5-shim.js` monkey-patches a JavaScript context to contain all EcmaScript 5
- * methods that can be faithfully emulated with a legacy JavaScript engine.
- *
- * `es5-sham.js` monkey-patches other ES5 methods as closely as possible.
- * For these methods, as closely as possible to ES5 is not very close.
- * Many of these shams are intended only to allow code to be written to ES5
- * without causing run-time errors in older engines. In many cases,
- * this means that these shams cause many ES5 methods to silently fail.
- * Decide carefully whether this is what you want. Note: es5-sham.js requires
- * es5-shim.js to be able to work properly.
- *
- * `json3.js` monkey-patches the EcmaScript 5 JSON implimentation faithfully.
- *
- * `es6.shim.js` provides compatibility shims so that legacy JavaScript engines
- * behave as closely as possible to ECMAScript 6 (Harmony).
- *
- * @version 1.1.0
+ * @version 1.1.1
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module is-function-x
  */
 
-/* jslint maxlen:80, es6:true, white:true */
-
-/* jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
-   freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
-   nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
-   es3:false, esnext:true, plusplus:true, maxparams:1, maxdepth:1,
-   maxstatements:3, maxcomplexity:2 */
-
-/* eslint strict: 1, max-statements: 1 */
+/* eslint strict: 1 */
 
 /* global module */
 
@@ -70,6 +45,7 @@
   var isPrimitive = require('is-primitive');
   var funcTag = '[object Function]';
   var genTag = '[object GeneratorFunction]';
+  var asyncTag = '[object AsyncFunction]';
 
   /**
    * Checks if `value` is classified as a `Function` object.
@@ -79,7 +55,7 @@
    * @return {boolean} Returns `true` if `value` is correctly classified,
    * else `false`.
    */
-  var tryFunctionObject = function (value) {
+  var tryFunctionObject = function funcToString(value) {
     try {
       fToString.call(value);
       return true;
@@ -116,6 +92,6 @@
       return tryFunctionObject(value);
     }
     var strTag = toStringTag(value);
-    return strTag === funcTag || strTag === genTag;
+    return strTag === funcTag || strTag === genTag || strTag === asyncTag;
   };
 }());
