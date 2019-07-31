16dbf793aa9d2332a49b632071be2bbb32845539
Graham Fairweather <xotic750@gmail.com>
2019-07-31 19:14:57 +0200

    Package rework

--- dist/is-function-x.js
+++ dist/is-function-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-07-31T15:45:21.076Z",
+  "date": "2019-07-31T17:14:51.826Z",
   "describe": "",
   "description": "Determine whether a given value is a function object.",
   "file": "is-function-x.js",
