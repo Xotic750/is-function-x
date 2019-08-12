ef85f6f31d90fecc9f5edca600f247ae6c1ec95c
Graham Fairweather <xotic750@gmail.com>
2019-08-12 23:54:33 +0200

    :bookmark: v4.0.28
    Auto run

--- dist/is-function-x.js
+++ dist/is-function-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-08-07T21:54:52.372Z",
+  "date": "2019-08-12T21:54:27.405Z",
   "describe": "",
   "description": "Determine whether a given value is a function object.",
   "file": "is-function-x.js",
